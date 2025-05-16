cbcef31968453357b3d0bce50b82b68d333ae092
rahulrao6610 <22bk1a6610@stpetershyd.com>
2025-05-16 12:06:15 +0530

    recent

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/NumberGuessingGame-for-Micro-IT/', 
+  base: '/NumberGuessingGame-for-Micro-IT/',
   plugins: [react()],
 })
